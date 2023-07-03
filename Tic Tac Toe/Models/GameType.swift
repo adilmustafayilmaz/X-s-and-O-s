59cdfb7a10e8126ba13ddfafab559c7f85a08de5
adilmustafayilmaz <79438419+adilmustafayilmaz@users.noreply.github.com>
2023-07-03 10:26:06 +0300

    Game Planning Completed

--- Tic Tac Toe/Models/GameType.swift
+++ Tic Tac Toe/Models/GameType.swift
@@ -5,7 +5,7 @@
 //  Created by Adil Mustafa Yılmaz on 3.07.2023.
 //
 
-import Foundation
+import SwiftUI
 
 enum GameType{
     case single, bot, peer, undermined
@@ -23,4 +23,44 @@ enum GameType{
             return ""
         }
     }
+}
+
+
+enum GamePiece: String {
+    case x, o
+    var image: Image {
+        Image(self.rawValue)
+    }
+}
+
+
+struct Player {
+    let gamePiece: GamePiece
+    var name: String
+    var moves: [Int] = []
+    var isCurrent = false
+    
+    var isWinner: Bool {
+        for moves in Move.winningMoves{
+            if moves.allSatisfy(self.moves.contains){
+                return true
+            }
+        }
+        return false
+    }
+}
+
+enum Move {
+    static var all = [1,2,3,4,5,6,7,8,9]
+
+    static var winningMoves = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [1,4,7],
+        [2,5,8],
+        [3,6,9],
+        [1,2,9],
+        [3,5,7]
+    ]
 }
